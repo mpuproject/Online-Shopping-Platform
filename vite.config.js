a16c02d6e499f9dd2be4387650046794ca6acb36
Hanson5732 <1198491837@qq.com>
2025-04-03 00:34:21 +0800

    Update config

--- vite.config.js
+++ vite.config.js
@@ -35,7 +35,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         secure: false,
